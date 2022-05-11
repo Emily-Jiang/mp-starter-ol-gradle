41053ad2bf7255b7abd8b28edd4049af5d676ccb
Emily-Jiang <emijiang6@googlemail.com>
2022-05-11 22:23:17 +0100

    add files

---
+++ service-a/src/main/java/com/example/demodry/DemodryRestApplication.java
@@ -0,0 +1,11 @@
+package com.example.demodry;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class DemodryRestApplication extends Application {
+}
